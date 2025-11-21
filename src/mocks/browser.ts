521c19a7a6678e2286363559b41c66ba6d30bd27
Jhorman Orozco <jsojhor@gmail.com>
2025-11-21 00:06:56 -0500

    refactor: remove mock service worker files

--- src/mocks/browser.ts
+++
@@ -1,5 +0,0 @@
-import { setupWorker } from "msw/browser";
-import { handlers } from "./handlers";
-
-// Configura el service worker para el navegador
-export const worker = setupWorker(...handlers);
