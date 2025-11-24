c3b7a45881409432501a3a4e134d79824e10e8ec
Jhorman Orozco <jsojhor@gmail.com>
2025-11-24 12:13:10 -0500

    chore: add Jest config, setupTests, tsconfig types and sample test

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+// jest.config.ts
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/*.d.ts'],
+};
+export default config;
